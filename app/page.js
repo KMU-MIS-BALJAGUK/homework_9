56ad769934d251d04eb00538968faf8e11867022
황재빈 <hwnagjaebinh@gmail.com>
2025-09-15 15:01:16 +0900

    second commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center h-screen items-center">
+      메인 페이지입니다.
+    </div>
+  );
+}
